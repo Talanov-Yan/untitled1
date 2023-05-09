9f0b7fe93d1a34cc82b7cf57f1a43292bd5be39b
Talanov-Yan <yan.talanov@bk.ru>
2023-05-09 18:03:35 +0300

    commit 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! ветка test");
         System.out.println("test");
+        System.out.println("мои изменени");
     }
 }
