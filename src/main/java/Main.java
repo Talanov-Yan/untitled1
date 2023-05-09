0c3c3cfca822cf362530ec53993c7fa4a3921864
Talanov-Yan <yan.talanov@bk.ru>
2023-05-09 17:47:24 +0300

    commit 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello World! ветка маин");
         System.out.println("Test1");
         System.out.println("Ветка тест");
+        System.out.println("reset main");
     }
 }
