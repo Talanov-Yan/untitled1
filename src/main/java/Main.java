044ac2a4eaf903bcac4edbd1349215ead36893ae
Talanov-Yan <yan.talanov@bk.ru>
2023-05-09 18:01:48 +0300

    commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! ветка маин");
-        System.out.println("Test1");
-        System.out.println("Ветка тест");
-        System.out.println("reset main нужно добавить в тест");
-        System.out.println(" ");
+        System.out.println("Hello World! ветка test");
     }
 }
